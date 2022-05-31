db9d62784886bd8f3c1162e8277fde92adba1dd8
n8thanael <n8thanael@hotmail.com>
2022-05-31 19:32:25 -0400

    simple test

--- wsgi_test_.py
+++ wsgi_test_.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
+    message = 'I can\'t believe this works!\n'
     version = 'Python %s\n' % sys.version.split()[0]
     response = '\n'.join([message, version])
     return [response.encode()]
