5169c8caafc18f9bfd3da020a4410d250e7e7bfc
n8thanael <n8thanael@hotmail.com>
2022-06-07 20:05:38 -0400

    homepage with bootstrap is functional

--- misfits/views.py
+++ misfits/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# we need to pass this request
+def home(request):
+    var = "Home"
+    # context variables in the {} are accessible on home within {{}}
+    return render(request, 'home.html',{'var':var,})
