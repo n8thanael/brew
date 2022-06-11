017b5e024a7d1f2ed5f909a28fb4f1f08897f367
n8thanael <n8thanael@hotmail.com>
2022-06-10 22:55:28 -0400

    capitalized file suffixes were blowing up everything - sheesh

--- misfits/views.py
+++ misfits/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # we need to pass this request
 def home(request):
-    var = "Home"
-    # context variables in the {} are accessible on home within {{}}
-    return render(request, 'home.html',{'var':var,})
+    return render(request, 'home.html')
 
 # Create your views here.
 # we need to pass this request
